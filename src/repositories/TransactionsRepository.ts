8425fb4dca9f64f13a2c3ae13bd7eb9ed717593e
Alisson Zotarelli <alzo.zotarelli@gmail.com>
2021-01-27 18:50:12 -0300

    I finished my test. Now I'm seending to RocketSeat

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,16 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
+interface ResponseBalance {
+  transactions: Array<Transaction>;
+  balance: Balance;
+}
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -15,14 +25,33 @@ class TransactionsRepository {
 
   public all(): Transaction[] {
     // TODO
+    return this.transactions;
   }
 
-  public getBalance(): Balance {
-    // TODO
+  public getBalance(): ResponseBalance {
+    const balance: Balance = { income: 0, outcome: 0, total: 0 };
+
+    this.transactions.forEach(transaction => {
+      switch (transaction.type) {
+        case 'income':
+          balance.income += transaction.value;
+          break;
+        case 'outcome':
+          balance.outcome += transaction.value;
+          break;
+        default:
+      }
+    });
+    balance.total = balance.income - balance.outcome;
+
+    return { transactions: this.transactions, balance };
   }
 
-  public create(): Transaction {
+  public create({ title, type, value }: CreateTransactionDTO): Transaction {
     // TODO
+    const transaction = new Transaction({ title, type, value });
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,11 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface RequestDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +13,19 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, type, value }: RequestDTO): Transaction {
+    if (type === 'outcome') {
+      const { balance } = this.transactionsRepository.getBalance();
+      if (value > balance.total) {
+        throw new Error(`You don't have money to spend!`);
+      }
+    }
+    const transaction = this.transactionsRepository.create({
+      title,
+      type,
+      value,
+    });
+    return transaction;
   }
 }
 
